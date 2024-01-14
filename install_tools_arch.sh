27ea7444f4e8a5c0abb3f0f7628eb06c3e972660
Adam Thompson <adam@serialphotog.com>
2024-01-14 15:42:16 -0700

    Added more tools

--- install_tools_arch.sh
+++ install_tools_arch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# This is intended to be ran on a new Arch install to do some of the initial
+# setup of the box.
 
-sudo pacman -S git gcc gdb vim make zsh tmux vscode obsidian
+# Install basic tools from repos
+sudo pacman -S git gcc gdb vim make zsh tmux vscode obsidian binutils nmap
 yay -S oh-my-zsh-git google-chrome ghidra-git spotify
