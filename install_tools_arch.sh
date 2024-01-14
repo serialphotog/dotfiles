b84cf412bd7c0d976c43a3f1804ba6fc4b801884
Adam Thompson <adam@serialphotog.com>
2024-01-14 16:03:52 -0700

    Added virtualization packages

--- install_tools_arch.sh
+++ install_tools_arch.sh
@@ -5,5 +5,5 @@
 
 # Install basic tools from repos
 sudo pacman -S git gcc gdb vim make zsh tmux vscode obsidian binutils nmap
-sudo pacman -S wireshark-qt john
+sudo pacman -S wireshark-qt john libvirt virt-manager qemu-full
 yay -S oh-my-zsh-git google-chrome ghidra-git spotify
