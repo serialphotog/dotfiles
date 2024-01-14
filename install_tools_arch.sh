1a03638e01f6337e95f7fa66d2c6425fc2ce3ac2
Adam Thompson <adam@serialphotog.com>
2024-01-14 15:23:44 -0700

    Added more tools

--- install_tools_arch.sh
+++ install_tools_arch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pacman -S git gcc gdb vim make zsh tmux
-yay -S oh-my-zsh-git
+sudo pacman -S git gcc gdb vim make zsh tmux vscode obsidian
+yay -S oh-my-zsh-git google-chrome ghidra-git spotify
