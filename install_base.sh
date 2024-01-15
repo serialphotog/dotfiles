a9d51c30a5a44740099317902ff3ded0d8316715
Adam Thompson <adam@serialphotog.com>
2024-01-15 11:50:01 -0700

    Fixed typo in .gitconfig

--- install_base.sh
+++ install_base.sh
@@ -8,6 +8,6 @@ ln -s /home/$USER/.dotfiles/.vimrc /home/$USER/.vimrc
 ln -s /home/$USER/.dotfiles/.tmux.conf /home/$USER/.tmux.conf
 ln -s /home/$USER/.dotfiles/.gitignore_global /home/$USER/.gitignore_global
 ln -s /home/$USER/.dotfiles/.gitignore /home/$USER/.gitignore
-ln -s /home/$USER/.dotfiles/.giconfig /home/$USER/.gitconfig
+ln -s /home/$USER/.dotfiles/.gitconfig /home/$USER/.gitconfig
 ln -s /home/$USER/.dotfiles/.gdbinit /home/$USER/.gdbinit
 ln -s /home/$USER/.dotfiles/.aliases.zsh /home/$USER/.aliases.zsh
