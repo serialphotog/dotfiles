7de6612f68fab369b87ee2249190828c546c23cf
Adam Thompson <adam@serialphotog.com>
2024-01-15 11:41:10 -0700

    Added htop

--- install_tools_arch.sh
+++ install_tools_arch.sh
@@ -12,6 +12,6 @@
 # Install basic tools from repos
 sudo pacman -S git gcc gdb vim make zsh tmux vscode obsidian binutils nmap \
 wireshark-qt john libvirt virt-manager qemu-full tor torbrowser-launcher \
-hugo opensnitch radare2 rz-cutter
+hugo opensnitch radare2 rz-cutter htop
 
 yay -S oh-my-zsh-git google-chrome ghidra-git spotify
