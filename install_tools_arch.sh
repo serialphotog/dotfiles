73d0ed5666e35ce27d86fa940a433fd7e88e3e33
Adam Thompson <adam@serialphotog.com>
2024-01-14 18:25:50 -0700

    Added more tools

--- install_tools_arch.sh
+++ install_tools_arch.sh
@@ -5,5 +5,6 @@
 
 # Install basic tools from repos
 sudo pacman -S git gcc gdb vim make zsh tmux vscode obsidian binutils nmap
-sudo pacman -S wireshark-qt john libvirt virt-manager qemu-full tor
+sudo pacman -S wireshark-qt john libvirt virt-manager qemu-full tor torbrowser-launcher
+sudo pacman -S hugo opensnitch radare2 rz-cutter
 yay -S oh-my-zsh-git google-chrome ghidra-git spotify
