b2bc7382efec12c21899e806540a506492e68361
Adam Thompson <adam@serialphotog.com>
2024-01-19 16:47:04 -0700

    Add local aliases to Kali

--- install_kali_wsl.sh
+++ install_kali_wsl.sh
@@ -11,3 +11,6 @@ sudo apt update && sudo apt install -y kali-linux-default kali-win-kex
 # Install Plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Add aliases to Kali's ZSH config
+echo 'source ~/.aliases.local' >> ~/.bashrc
