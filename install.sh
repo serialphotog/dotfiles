7e5d3e64f4baffd64294011f4382ddfc575a0a7e
Adam Thompson <adam@hackeradam.com>
2026-02-17 07:38:22 -0700

    Add ZSH and oh-my-zsh

--- install.sh
+++ install.sh
@@ -7,8 +7,12 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 # Install base packages that I'll want
 echo -e "${GREEN}[Info]: Installing base Fedora Linux packages${CLEAR}"
-sudo dnf install wget cargo fd kitty gcc gdb git binutils binwalk
+sudo dnf install zsh wget cargo fd kitty gcc gdb git binutils binwalk
 cargo install eza
+
+# Install oh-my-zsh
+echo -e "${GREEN}[Info]: Installing oh-my-zsh${CLEAR}"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Run the install script
 echo -e "${GREEN}[Info]: Running the base install script${CLEAR}"
